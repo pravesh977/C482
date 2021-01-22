73e76e5de7f33afc7606648f41d4dd78ec25d472
pravesh <praveshshrestha@Praveshs-MBP.fios-router.home>
2021-01-22 09:46:07 -0500

    created all classes from uml diagram

---
+++ src/model/Inventory.java
@@ -0,0 +1,57 @@
+package model;
+
+import javafx.collections.ObservableList;
+
+public class Inventory {
+
+    private ObservableList<Part> allParts;
+    private ObservableList<Product> allProducts;
+
+    public void addPart (Part newPart) {
+
+    }
+
+    public void addProduct (Product newProduct) {
+
+    }
+
+//    public Part lookupPart (int partId) {
+//        return somepart
+//    }
+//
+//    public Product lookupProduct (int productId) {
+//        return someproduct
+//    }
+//
+//    public ObservableList<Part> lookupPart (String partName) {
+//        return list of parts
+//    }
+//
+//    public ObservableList<Product> lookupProduct (String productName) {
+//        return list of products
+//    }
+//
+//    public void updatePart (int index, Part selectedPart) {
+//
+//    }
+//
+//    public void updateProduct (int index, Product newProduct) {
+//
+//    }
+//
+//    public boolean deletePart (Part selectedPart) {
+//        return ture or false;
+//    }
+//
+//    public boolean deleteProduct (Product selectedProduct) {
+//        return true or false;
+//    }
+//
+//    public ObservableList<Part> getAllParts () {
+//        return some list
+//    }
+//
+//    public ObservableList<Product> getAllProducts () {
+//        return some list
+//    }
+}
