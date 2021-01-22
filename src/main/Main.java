0a182a01b83a8127a0be5b6ca47d6868ef4e02ff
pravesh <praveshshrestha@Praveshs-MBP.fios-router.home>
2021-01-21 21:56:07 -0500

    creating different views

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/mainview.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/main_screen.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
