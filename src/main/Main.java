189e02b87f6e4f2836234972c6baf0a8a49c6a5e
pravesh <praveshshrestha@Praveshs-MBP.fios-router.home>
2021-01-23 10:40:09 -0500

    created the first main page

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,19 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Part;
+import model.Product;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/main_screen.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("C482 Inventory Management System WGU");
+        primaryStage.setScene(new Scene(root, 1020, 400));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+//        System.out.println("hello boy");
+//        Product bike = new Product(12, "BMX", 120, 15, 5, 20);
+//        System.out.println(bike.getName());
         launch(args);
 
     }
